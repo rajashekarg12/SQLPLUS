74a9d068a4c72eff37763d923832fb455fdb4cf4
Rajashekarg12 <110624878+Rajashekarg12@users.noreply.github.com>
2024-06-11 13:39:47 +0530

    Create compilewarning.sql

---
+++ compilewarning.sql
@@ -0,0 +1,24 @@
+set serveroutput on
+CREATE OR REPLACE PROCEDURE generate_warnings AS
+BEGIN
+    DECLARE
+        v_variable NUMBER;
+    BEGIN
+        -- This assignment will cause an "unused variable" warning
+        v_variable := 10;
+        
+        -- This statement will cause a "procedure created with compilation errors" warning
+        SELECT column_name
+        INTO v_variable
+        FROM non_existing_table;
+        
+        -- This statement will cause a "no_data_found" warning
+        SELECT column_name
+        INTO v_variable
+        FROM your_table
+        WHERE 1 = 2;
+        
+    END;
+END;
+SHOW ERRORS;
+/
