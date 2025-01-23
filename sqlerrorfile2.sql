f474227fb61e31f47b6cf3be1b33f60a6440454a
Avnish Patel <110018459+Avnishpat@users.noreply.github.com>
2025-01-23 18:35:59 +0530

    Create sqlerrorfile2.sql

---
+++ sqlerrorfile2.sql
@@ -0,0 +1,12 @@
+
+-- Create a table
+CREATE TABLE employees (
+    id NUMBER PRIMARY KEY,
+    name VARCHAR2(5),
+    department VARCHAR2(50),
+    salary NUMBER
+);
+
+-- Insert a row into the table
+INSERT INTO employees (id, name, department, salary) 
+VALUES (1, 'Trump Donald', 'HR', 50000);
