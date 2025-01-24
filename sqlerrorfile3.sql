9616d10b939db1914f5d7d40376eea769688e326
Avnish Patel <110018459+Avnishpat@users.noreply.github.com>
2025-01-24 09:45:46 +0530

    Update sqlerrorfile3.sql

--- sqlerrorfile3.sql
+++ sqlerrorfile3.sql
@@ -1,12 +1,12 @@
 
 -- Create a table
-CREATE TABLE President (
+/*CREATE TABLE President (
     id NUMBER PRIMARY KEY,
     name VARCHAR2(5),
     department VARCHAR2(50),
     salary NUMBER
 );
-
+*/
 -- Insert a row into the table
 INSERT INTO President (id, name, department, salary) 
-VALUES (1, 'MODI', 'PM', 50000.00.00);
+VALUES (2, 'MODIJI', 'PM', 50000000000000000000);
