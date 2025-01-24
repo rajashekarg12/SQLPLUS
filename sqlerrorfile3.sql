d8d584f8f368d3df723a77248ea6905a5debb1c0
Avnish Patel <110018459+Avnishpat@users.noreply.github.com>
2025-01-24 09:38:29 +0530

    Create sqlerrorfile3.sql

---
+++ sqlerrorfile3.sql
@@ -0,0 +1,12 @@
+
+-- Create a table
+CREATE TABLE President (
+    id NUMBER PRIMARY KEY,
+    name VARCHAR2(5),
+    department VARCHAR2(50),
+    salary NUMBER
+);
+
+-- Insert a row into the table
+INSERT INTO employees (id, name, department, salary) 
+VALUES (1, 'MODI', 'PM', 50000.00);
